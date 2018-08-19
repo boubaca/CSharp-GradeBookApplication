94c67efc0b6311686010207364c1503c161b5607
Boubacar Barry <meriambb1@gmail.com>
2018-08-18 22:05:48 -0400

    parcentile

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -11,23 +12,20 @@ namespace GradeBook.GradeBooks
             Type = Enums.GradeBookType.Ranked;
         }
 
-        override
-        public char GetLetterGrade(double averageGrade)
+        
+        public override char GetLetterGrade(double averageGrade)
         {   
             if(Students.Count < 5)
-            {
-                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
-            }
-            if (averageGrade >= 90)
-                return 'A';
-            else if (averageGrade >= 80)
-                return 'B';
-            else if (averageGrade >= 70)
-                return 'C';
-            else if (averageGrade >= 60)
-                return 'D';
-            else
-                return 'F';
+              throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+
+            var parcentage = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(n => n.AverageGrade).Select(n =>n.AverageGrade).ToList();
+
+            if (grades[parcentage - 1] <= averageGrade) return 'A';
+            else if (grades[parcentage * 2 - 1] <= averageGrade) return 'B';
+            else if (grades[parcentage * 3 - 1] <= averageGrade) return 'C';
+            else if (grades[parcentage * 4 - 1] <= averageGrade) return 'D';
+            else return 'F';
 
            
 
